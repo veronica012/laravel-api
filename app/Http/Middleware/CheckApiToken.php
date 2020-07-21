c0a1e1859b4c9367add51429a7e44f4e82123ba4
Veronica Celseti <veronicacelseti12@gmail.com>
2020-07-21 16:32:04 +0200

    controlli sul token

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use App\User;
 class CheckApiToken
 {
     /**
@@ -15,6 +15,28 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
+        $auth_header = $request->header('authorization');
+        //verifico che ci sia un header di autorizzazione se è presente va avanti
+        if(empty($auth_header)) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api Token mancante'
+            ]);
+        }
+
+        //per verificare che il token sia valido recupero il Token
+        $api_token = substr($api_token, 7);
+
+        $user = User::where('api_token', $api_token)->first();
+
+        if(empty($user)) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api Token errato'
+            ]);
+        }
+        //se è falso manda avanti la richiesta
         return $next($request);
+
     }
 }
